7b623b08bef24c4055fa5855e42d9b1dbf9b1c81
Pranita Mantravadi <pranitamantravadi@Pranitas-MacBook-Pro.local>
2020-03-03 18:33:54 -0800

    added URL submission functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Typography, TextField, Grid, Button, AppBar, Toolbar, IconButton, Tabs, Tab, } from '@material-ui/core';
 import MenuIcon from "@material-ui/icons/Menu";
 import './App.css';
@@ -10,14 +10,33 @@ function App() {
   const logoStyle = { height: '50px'};
   const payPalStyle = {marginRight: '30px'};
   const tabsStyle = {flexGrow: 1};
+
+  function handleChange(event) {
+    console.log('Function was called') 
+    console.log(event.target.value)
+    setTestURL(event.target.value)
+  }
   
+  async function handleClick() {
+    console.log('Button was clicked')
+
+    const url = "http://impostor-blocker.com/v1/" + testURL + "/[brandid]"
+
+    const response = await fetch(url)
+    const text = await response.text();
+
+    console.log(text);
+  }
+
+  const [testURL, setTestURL] = useState('')
+
   return (
     <div>
       <AppBar position="static">
         <Toolbar>
           <img src={logo} style={logoStyle}/>
-          <Tabs style={tabsStyle}>
-            <Tab label="Fraud History"/>
+          <Tabs value={0} style={tabsStyle}>
+            <Tab label="Home"/>
             <Tab label="Fraud History"/>
             <Tab label="Fraud History"/>
           </Tabs>
@@ -32,15 +51,15 @@ function App() {
         </Typography>
         <Grid container spacing={3} alignItems='center'>
           <Grid item xs={10}>
-            <TextField variant="filled" fullWidth />
+            <TextField variant="filled" fullWidth value={testURL} onChange={handleChange}/>
           </Grid>
           <Grid item xs={2}>
-            <Button variant="contained" size='large'>Submit</Button>
+            <Button variant="contained" size='large' onClick={handleClick}>Submit</Button>
           </Grid>
         </Grid>
       </div>
     </div>
-  );
+  )
 }
 
 
